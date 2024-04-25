f1300be175054b8a5fb88e60be12afe5f9e79334
forgottengd <konsts@yandex.ru>
2024-04-25 12:02:36 +0300

    Final fixes

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import base64
 import os
 import requests
 import streamlit as st
-from openai import OpenAI
 
 
-client = OpenAI()
 OCR_API = os.environ['OCR_API']
+folder_id = "b1g79vtsge6ggong8ioa"
 
 
 def encode_file_to_base64(file):
@@ -17,7 +16,6 @@ def encode_file_to_base64(file):
 
 def send_ocr_request(iam_token, encoded_image):
     """ Отправляет OCR запрос и возвращает результат в json формате. """
-    folder_id = "b1g79vtsge6ggong8ioa"
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Api-Key {iam_token}",
@@ -36,15 +34,52 @@ def send_ocr_request(iam_token, encoded_image):
     return response.json()
 
 
+def send_gpt_request(ocr_token, ocr_text, temp):
+    """ Отправляет GPT запрос и возвращает результат в json формате. """
+    print(ocr_text)
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Api-Key {ocr_token}",
+        "x-folder-id": folder_id,
+    }
+
+    body = {
+        "modelUri": f"gpt://{folder_id}/yandexgpt/latest",
+        "completionOptions": {
+            "stream": False,
+            "temperature": temp,
+            "maxTokens": "100"
+        },
+        "messages": [
+            {
+                "role": "system",
+                "text": "Проведи глубокий анализ текста и внимательно посмотри, является ли он перепиской. "
+                        "Используй эти признаки переписки: текст выглядит как диалог или общение, "
+                        "в нем есть временные метки сообщений, приветствия, прощания, или какое-то общение."
+                        "Ответь одним и только одним словом: да или нет"
+            },
+            {
+                "role": "user",
+                "text": ocr_text
+            }
+        ]
+    }
+
+    response = requests.post("https://llm.api.cloud.yandex.net/foundationModels/v1/completion", headers=headers,
+                             json=body)
+    return response.json()
+
+
 def main():
+    """ Создает объекты streamlit-сервиса """
     uploader = st.file_uploader("Choose image for detection", type=['png', 'jpg', 'jpeg'])
     if uploader:
         slider_value = st.slider(
             'Temperature',
-            min_value=0.0,  # минимальное значение
-            max_value=2.0,  # максимальное значение
-            step=0.1,  # шаг изменения
-            value=1.0  # начальное значение
+            min_value=0.0,
+            max_value=1.0,
+            step=0.1,
+            value=0.4
         )
         detect_button = st.empty()
         if detect_button.button("Detect"):
@@ -52,14 +87,8 @@ def main():
             on_click(uploader, slider_value)
 
 
-def summary_prompt(ocr_text, blocks) -> str:
-    text = ("There is text, which I got by optical character recognition. You should do deep analyse "
-            "and understand is it converstaion or not. Remember, you should answer with one "
-            f"and only one word: yes or no. Text to analyse: {ocr_text}")
-    return text
-
-
 def on_click(file, temp):
+    """ Вызывает обработку загруженного изображения и отображает результат """
     full_text = ""
     with st.spinner("Detecting image..."):
         try:
@@ -75,23 +104,12 @@ def on_click(file, temp):
             if full_text == "":
                 full_text = "There is no conversation on image"
             else:
-                response = client.chat.completions.create(
-                    model="gpt-3.5-turbo",
-                    messages=[
-                        {
-                            "role": "user",
-                            "content": summary_prompt(full_text, blocks_number),
-                        }
-                    ],
-                    max_tokens=300,
-                    temperature=temp,
-                )
-                full_text = response.choices[0].message["content"]
+                gpt_response = send_gpt_request(OCR_API, full_text, temp)
+                full_text = gpt_response['result']['alternatives'][0]['message']['text']
         except Exception as e:
             st.error(f"Error: {e}")
             st.stop()
 
-    # st.text_area(label="Result", value=response.choices[0].message["content"], height=300)
     st.text_area(label="Result", value=full_text, height=300)
 
 
